b8096134b6ed18d6528f7b3c600fd879d8990821
Nick Melloria <nickxylanmelloria@gmail.com>
2024-02-18 11:16:57 +0800

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-950 min-h-screen w-full">
+      <h1 className="text-white text-5xl">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
